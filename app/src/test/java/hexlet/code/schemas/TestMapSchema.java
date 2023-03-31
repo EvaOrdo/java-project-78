6b74e95e5ee4b4a8a34b5703155543fac6187f5f
Hanna Mudrak <annmudrak@gmail.com>
2023-03-31 14:05:00 -0500

    add test for shape

--- app/src/test/java/hexlet/code/schemas/TestMapSchema.java
+++ app/src/test/java/hexlet/code/schemas/TestMapSchema.java
@@ -45,5 +45,36 @@ public class TestMapSchema {
         data.put("key2", "value2");
         assertTrue(schema.isValid(data));
     }
+    @Test
+    public void testShape() {
 
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        StringSchema stringSchema = new StringSchema();
+        NumberSchema numberSchema = new NumberSchema();
+
+        schemas.put("name", stringSchema.required());
+        schemas.put("age", numberSchema.positive());
+
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertTrue(schema.isValid(human1));
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertTrue(schema.isValid(human2));
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertFalse(schema.isValid(human3));
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        assertFalse(schema.isValid(human4));
+    }
 }
